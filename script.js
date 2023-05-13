234fe6ae9e84a92481c1a06255893691da0bbc9f
nathandev254 <warimwenathaniel@gmail.com>
2023-05-13 10:25:43 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 const submit = document.querySelector(".Submit");
-const inputname = document.querySelector("#name").value;
-const inputnumber = document.querySelector("#idNumber").value;
-const country = document.querySelector("#country");
-const language = document.querySelector("#Language");
 
-const countryselected = country.value
-const languageselected = language.value
-
-const Herocontainer = document.querySelector(".container--hero");
-
-function Display(inputname = 'nathan', inputnumber, country, language) {
-  const html = `<div class="details">
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  const inputname = document.querySelector(".name").value;
+  const inputnumber = document.querySelector(".idNumber").value;
+  const selectcountry = document.querySelector("#country").value;
+  const selectlanguage = document.querySelector("#Language").value;
+  const Herocontainer = document.querySelector(".container--hero");
+  // console.log(inputname,inputnumber,selectcountry,selectlanguage)
+  if (
+    ((inputname == ""),
+    (inputnumber == ""),
+    (selectcountry == ""),
+    (selectlanguage == ""))
+  ) {
+    return alert(`enter the relevant details`);
+  } 
+  else {
+    const html = `<div class="details">
   <p class="output">${inputname}</p>
   <p class="output">${inputnumber}</p>
-  <p class="output">${country}</p>
-  <p class="output">${language}</p>
+  <p class="output">${selectcountry}</p>
+  <p class="output">${selectlanguage}</p>
   <button class="Delete">Delete</button>
 </div>`;
-  Herocontainer.insertAdjacentHTML("beforeend", html);
-  inputname,inputnumber,country,language = '';
-}
-
-submit.addEventListener('click', (e) =>{
-    e.preventDefault()
-    console.log(inputname,inputnumber)
-    Display(inputname,inputnumber,countryselected,languageselected)
-
-})
+    Herocontainer.insertAdjacentHTML("beforeend", html);
+  }
+});
